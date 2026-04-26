1395e5b0ed4aa5c669ed9b0a16e6e4b4f440272b
Benjamin <matapobenjamin28@gmail.com>
2026-04-26 20:30:07 +0100

    build(web): add lightweight Apollo client typings shim to unblock local building

---
+++ apps/web/src/types/apollo.d.ts
@@ -0,0 +1,17 @@
+declare module '@apollo/client' {
+  // Minimal shims to allow TypeScript compilation in environments
+  // where the real type definitions are not being picked up correctly.
+  export const ApolloProvider: any;
+  export class ApolloClient<T = any> {
+    constructor(...args: any[]);
+  }
+  export const InMemoryCache: any;
+  export function HttpLink(args?: any): any;
+  export function split(test: any, left: any, right: any): any;
+  export function useQuery(...args: any[]): any;
+  export function useMutation(...args: any[]): any;
+  export function useSubscription(...args: any[]): any;
+  export const GraphQLWsLink: any;
+  export const createHttpLink: any;
+  export const ApolloLink: any;
+}
