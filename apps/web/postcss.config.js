47eddcce2983e5b369345683820eb4aa3de484af
Benjamin <matapobenjamin28@gmail.com>
2026-04-26 20:46:13 +0100

    build(web): revert PostCSS plugin to tailwindcss (3.x) style for compatibility

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,4 +1,4 @@
-import tailwind from '@tailwindcss/postcss';
+import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 export default {
   plugins: [tailwind(), autoprefixer()],
