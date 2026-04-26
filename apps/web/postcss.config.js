5b54e89a1609b17236cb208a787511db3fc766b5
Benjamin <matapobenjamin28@gmail.com>
2026-04-26 20:45:20 +0100

    build(web): fix PostCSS config for ESModule environment by using ES imports

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,6 +1,5 @@
+import tailwind from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 export default {
-  plugins: [
-    require('@tailwindcss/postcss')(),
-    require('autoprefixer')(),
-  ],
+  plugins: [tailwind(), autoprefixer()],
 }
